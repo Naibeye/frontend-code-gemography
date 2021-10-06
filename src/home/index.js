b34fa6ad39d9e7c9d4a49405f6da967f7ffec359
Naibeye <dthekplus@gmail.com>
2021-10-06 08:24:36 +0000

    Gemography project Finalisation and documentation code

--- src/home/index.js
+++ src/home/index.js
@@ -1,27 +1,37 @@
+/**
+ *  @author DJIMNAIBEYE Sidoine
+ */
 import { useEffect , useState} from 'react';
 import RepositoriesService from '../services/SearchService';
 import moment  from 'moment';
 import ReposCard from './ReposCard';
 import { Box } from '@chakra-ui/react';
 
+/**
+ * main component: list the most starred Github repos that were created in the last 30 days.
+ */
 function Home() {
   const searchService=new RepositoriesService()
-  const [items, setItems]=useState([])
+  const [listRepos, setListRepos]=useState([])
   const [page, setPage] = useState(1);
 
-
+/**
+ * Detection the end of scroll
+ * @param {Event} e
+ */
   const onScroll = (e) => {
     if (e.target.scrollHeight-e.target.scrollTop===e.target.clientHeight){
         setPage(page+1)
     }
   };
-
+/**
+ * refresh page if Detect end of scroll.
+ */
   useEffect(()=>{
     const date= moment().subtract(30, "days").format("yyyy-MM-DD")
-    searchService.getRepositories(`q=created:>${date}&sort=stars&order=desc&page=${page}`,
+    searchService.getRepositories(`q=created:>${date}&sort=stars&order=desc&page=${page}&per_page=100`,
     response=>{
-      console.log(response)
-      setItems([...items, ...response.items])},
+      setListRepos([...listRepos, ...response.items])},
     error=>{
         console.log(error)
 
@@ -31,9 +41,9 @@ function Home() {
   return (
     <div onScroll={onScroll} style={{ height:"800px", overflowY: "scroll" }}>
     <Box alignContent="center">
-    {items
-      .map((item, index) => {
-        return <div key={index}><ReposCard  item={item}/></div>;
+    {listRepos
+      .map((repos, index) => {
+        return <div key={index}><ReposCard  repos={repos}/></div>;
       })}
       </Box>
   </div>
--- src/services/SearchService.js
+++ src/services/SearchService.js
@@ -1,11 +1,24 @@
+/**
+ *  @author DJIMNAIBEYE Sidoine
+ */
+
 import APIClient from "./APIClient";
 
 const PATH = 'repositories';
+/**
+ *  RepositoriesService class: find repositories via request
+ */
 export default class RepositoriesService {
 
     constructor() {
         this.client = APIClient.instance();
     }
+/**
+ *  getRepositories function
+ * @param {String} request
+ * @param {function} successCallback
+ * @param {function} errorCallback
+ */
     getRepositories(request, successCallback, errorCallback) {
         this.client.get(PATH, request, successCallback, errorCallback);
     }
